837f3d03430ad2c23bb2d0ef19da585d3e9f1411
Baiyang Wu <Baiyang Wu@Wu-Laptop.fios-router.home>
2018-11-12 20:15:42 -0500

    1

---
+++ SudokuLab5/SudokuBLL/src/main/java/pkgEnum/eGameDifficulty.java
@@ -0,0 +1,42 @@
+package pkgEnum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum eGameDifficulty {
+
+	EASY(100), MEDIUM(500), HARD(1000);
+	
+	private final int iDifficulty;
+	
+	private static final Map<Integer, eGameDifficulty> lookup = new HashMap<Integer,eGameDifficulty>();
+	
+	static {
+		for (eGameDifficulty d : eGameDifficulty.values()) {
+			lookup.put(d.getiDifficulty(),d);
+		}
+	}
+	
+	private eGameDifficulty(int iDifficulty) {
+		this.iDifficulty = iDifficulty;
+	}
+	
+	public int getiDifficulty() {
+		return iDifficulty;
+	}
+	
+	@Override
+	public String toString() {
+		return this.name().substring(0,1).toUpperCase()+this.name().substring(iDifficulty);
+	}
+	
+	public static eGameDifficulty get(String strName) {
+		for (eGameDifficulty eGD:eGameDifficulty.values()) {
+			System.out.println(eGD.name().toUpperCase());
+			if (eGD.name().toUpperCase().equals(strName.toUpperCase())){
+				return eGD;
+			}
+		}
+		return null;
+	}
+}
